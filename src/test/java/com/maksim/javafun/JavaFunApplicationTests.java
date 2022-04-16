2b8df96536255cb7b3270d5425d2429c59c27f99
= <mzmey37@gmail.com>
2022-04-16 12:49:30 +0200

    add test

--- src/test/java/com/maksim/javafun/JavaFunApplicationTests.java
+++ src/test/java/com/maksim/javafun/JavaFunApplicationTests.java
@@ -1,5 +1,8 @@
 package com.maksim.javafun;
 
+import com.maksim.javafun.model.Row;
+import com.maksim.javafun.step.RowProcessor;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,15 @@ class JavaFunApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void processorWorks() throws Exception{
+		RowProcessor rowProcessor = new RowProcessor();
+		Row row = new Row("1", "Hi");
+		Row processedRow = rowProcessor.process(row);
+		Assert.assertEquals("1", processedRow.getKey());
+		Assert.assertEquals("hi", processedRow.getValue());
 	}
 
 }
