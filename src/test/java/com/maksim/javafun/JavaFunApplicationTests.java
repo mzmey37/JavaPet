8b01ba35652998d649a2b2e8f3e429467e63c3d8
= <mzmey37@gmail.com>
2022-04-16 13:30:25 +0200

    Remove tests back

--- src/test/java/com/maksim/javafun/JavaFunApplicationTests.java
+++ src/test/java/com/maksim/javafun/JavaFunApplicationTests.java
@@ -1,8 +1,5 @@
 package com.maksim.javafun;
 
-import com.maksim.javafun.model.Row;
-import com.maksim.javafun.step.RowProcessor;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,15 +8,6 @@ class JavaFunApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void processorWorks() throws Exception{
-		RowProcessor rowProcessor = new RowProcessor();
-		Row row = new Row("1", "Hi");
-		Row processedRow = rowProcessor.process(row);
-		Assert.assertEquals("1", processedRow.getKey());
-		Assert.assertEquals("hi", processedRow.getValue());
 	}
 
 }
